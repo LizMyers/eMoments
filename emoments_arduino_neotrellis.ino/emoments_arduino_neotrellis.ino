baa5838d3c73ca13d40abd8d9a94924043625050
Liz Myers <liz@myersdesign.com>
2020-08-15 13:03:44 +0100

    Initial Commit

---
+++ emoments_arduino_neotrellis.ino/emoments_arduino_neotrellis.ino
@@ -0,0 +1,457 @@
+/**
+eMoments Implmentation by Liz Myers (@lizmyers)
+August 3, 2020
+
+HARDWARE
+1. Adafruit NeoTrellis 4x4 Grid
+2. Adafruit Huzzah Feather Esp32 (BlueTooth & WiFi)
+3. Adafruit Propmaker Feather Wing (using accelerometer to change modes randomly)
+4. LED Button (To Reset to Default mode)
+5. RESET Button (Destart script)
+6. Power Switch
+
+REFERENCE
+7. How to Setup Propmaker on Arduino
+https://learn.adafruit.com/adafruit-prop-maker-featherwing/arduino-code
+
+8. 3D Print on ThingiVerse (download STLs)
+https://www.thingiverse.com/thing:3750704
+
+9. CONCEPT 192
+Link to larger installation and art piece goes here
+
+ */
+ 
+#include "Adafruit_NeoTrellis.h" // Adafruit NeoTrellis 4x4 Grid
+
+#include <Adafruit_LIS3DH.h> // propmaker feather wing / accelerometer
+#include <Adafruit_Sensor.h> // propmaker feather wing / accelerometer
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+
+// ********************* These ones need to be adapted to your build *******************
+
+// How many leds in your strip?
+#define NUM_LEDS 16
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 4
+
+// Define LED type, e.g. NEOPIXEL, WS2811. The LED type can usually be found on the LED chip or is provided by the manufacturer.
+#define LED_TYPE NEOPIXEL
+
+// Define Mechanical Switch - LED Button Used
+#if defined(__SAMD21G18A__) || defined(__AVR_ATmega32U4__)
+  #define SWITCH_PIN   9
+#elif defined(__AVR_ATmega328P__)
+  #define SWITCH_PIN   9
+#elif defined(NRF52)
+  #define SWITCH_PIN   31
+#elif defined(ESP8266)
+  #define SWITCH_PIN   0
+#elif defined(TEENSYDUINO)
+  #define SWITCH_PIN   4
+#elif defined(ESP32)
+  #define SWITCH_PIN   15
+#endif
+
+// Available LED render patterns, e.g. sorted by creation time.
+enum LedRenderPattern {
+  RENDER_BY_COLOR = 0,
+  RENDER_BY_TIME = 1,
+  RENDER_RANDOMLY = 2,
+  RENDER_ONE_COLOR = 3,
+};
+//------------- Set the LED render pattern for this build. -------------------//
+
+// Testing Patterns and Setting Default
+//LedRenderPattern PATTERN = RENDER_ONE_COLOR;
+LedRenderPattern PATTERN = RENDER_RANDOMLY;
+//LedRenderPattern PATTERN = RENDER_BY_TIME;
+//LedRenderPattern PATTERN = RENDER_BY_COLOR;
+
+//***************************** END settings *********************************
+
+bool bluetoothConnected = false;
+bool sortByColor = false;
+bool sortByTime = false;
+bool sortRandomly = false;
+bool oneColor = false;
+bool randomMode = false;
+
+String colorString = "";
+
+//instantiate trellis 
+Adafruit_NeoTrellis trellis;
+
+//instantiate i2c accelerometer
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+//define a callback for key presses
+TrellisCallback blink(keyEvent evt){
+  
+  // Check is the pad pressed? Change display mode by pushing btns in cols 0 - 3
+  if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {
+    if (evt.bit.NUM == 0 || evt.bit.NUM == 4 || evt.bit.NUM == 8 || evt.bit.NUM == 12) {
+        sortByColor = true;
+        sortByTime= false;
+        oneColor = false;
+        sortRandomly = false;
+     } else if (evt.bit.NUM == 1 || evt.bit.NUM == 5 || evt.bit.NUM == 9 || evt.bit.NUM == 13) {
+        sortByColor = false;
+        sortByTime= true;
+        oneColor = false;
+        sortRandomly = false;
+     } else if (evt.bit.NUM == 2 || evt.bit.NUM == 6 || evt.bit.NUM == 10 || evt.bit.NUM == 14) {
+        sortByColor = false;
+        sortByTime= false;
+        oneColor = true;
+        sortRandomly = false;
+     } else if (evt.bit.NUM == 3 || evt.bit.NUM == 7 || evt.bit.NUM == 11 || evt.bit.NUM == 15) {
+        sortByColor = false;
+        sortByTime= false;
+        oneColor = false;
+        sortRandomly = true;
+     } 
+
+
+     //turn button white when pressed to acknowledge user interaction
+     trellis.pixels.setPixelColor(evt.bit.NUM, 0xFFFFFF); 
+  } else if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_FALLING) {
+  // or is the pad released?
+    Serial.println("Felt that button press!");
+    //trellis.pixels.setPixelColor(evt.bit.NUM, 0); //off falling
+  }
+  
+      //turn neopixels off btw display modes
+      for (uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
+        trellis.pixels.setPixelColor(i,0x000000);
+        trellis.pixels.show();
+        delay(50);
+      }
+
+  // Turn on/off the neopixels!
+  trellis.pixels.show();
+
+  return 0;
+}
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    bluetoothConnected = true;
+    Serial.println("BLUETOOTH CONNECTED <<< :-)");
+  };
+
+  void onDisconnect(BLEServer* pServer) {
+    bluetoothConnected = false;
+    colorString = "";
+    Serial.println("BLUE TOOTH DISCONNECTED <-:-( ");
+  }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      String value = pCharacteristic->getValue().c_str();
+      Serial.println("Obtained colors from bluetooth:");
+      Serial.println(value);
+      colorString = value;
+    }
+};
+
+
+void setup() {
+
+  // initing serial port
+  Serial.begin(115200);
+  Serial.println("\nProp-Maker Wing: Accelerometer");
+
+  // set up the switch
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  Serial.println("Setup LED Button...");
+ 
+  Serial.println("Starting LIS3DH...");
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start LIS3DH");
+    while (1);
+  }
+  Serial.println("LIS3DH Started!");
+ 
+  // Set Accelerometer Range (2, 4, 8, or 16 G!)
+  lis.setRange(LIS3DH_RANGE_4_G);
+  
+  trellis.begin();
+  trellis.pixels.setBrightness(90);
+  delay(2000);
+
+  //*********NeoTrellis Setup ***********//
+
+  //while(!Serial);
+  
+  if (!trellis.begin()) {
+    Serial.println("Could not start trellis, check wiring?");
+    while(1);
+  } else {
+    Serial.println("NeoPixel Trellis started");
+  }
+
+  //activate all keys and set callbacks
+  for(int i=0; i<NEO_TRELLIS_NUM_KEYS; i++){
+    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_RISING);
+    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_FALLING);
+    trellis.registerCallback(i, blink);
+  }
+
+  //do a little animation to show we're on
+  for (uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
+    trellis.pixels.setPixelColor(i,0xFFFFFF);
+    //trellis.pixels.setPixelColor(i, Wheel(map(i, 0, trellis.pixels.numPixels(), 0, 255)));
+    trellis.pixels.show();
+    delay(50);
+  }
+  //turn neopixels off
+  for (uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
+    trellis.pixels.setPixelColor(i,0x000000);
+    trellis.pixels.show();
+    delay(50);
+  }
+
+  // init bluetooth stuff
+  BLEDevice::init("InclusionMomentsDevice");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic  *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pCharacteristic->setValue("DefaultValue");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Bluetooth setup done!");
+}
+
+void loop() {
+
+  // Listen for a switch event
+  if (!digitalRead(SWITCH_PIN)) {
+    Serial.println("Switch pressed!");
+    
+//    int colorMode = (random(0, 3));
+//    Serial.print("colorMode: ");
+//    Serial.print(colorMode);
+  }
+  
+  trellis.read();
+  delay(2000);
+
+  showColorsFromString(colorString);
+  delay(2000);
+
+// Get a new accel. sensor event
+  sensors_event_t event; 
+  lis.getEvent(&event);
+
+  Serial.print("Accel. event: ");
+  Serial.println(&event);
+  Serial.println("");
+  delay(2000);
+
+  /* Display the results (acceleration is measured in m/s^2) */
+//  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+//  Serial.print(" \tY: "); Serial.print(event.acceleration.y); 
+//  Serial.print(" \tZ: "); Serial.print(event.acceleration.z); 
+//  Serial.println(" m/s^2 ");
+//  Serial.println();
+//  delay(500);
+  
+
+}
+
+
+//*************************** Start LED rendering ********************************
+
+// List of LED render functions. Each function in the list is defined below.
+typedef void (*LedRenderPatterns[])();
+LedRenderPatterns ledRenderPatterns = { renderByColor, renderByTime, renderRandomly, renderOneColor };
+
+// Shows the colors using the render function configured in PATTERN.
+void showColorsFromString(String colorString) {
+  Serial.print("Showing colors: ");
+  Serial.println(colorString);
+
+// Switch Patterns
+  if (colorString.length() > 0) { 
+      if (sortByColor) {
+        Serial.println("RENDER_BY_COLOR");
+        LedRenderPattern PATTERN = RENDER_BY_COLOR;
+        ledRenderPatterns[PATTERN]();
+        Serial.print("PATTERN = ");
+        Serial.println(PATTERN);
+      } else if (sortByTime) {
+        Serial.println("RENDER_BY_TIME");
+        LedRenderPattern PATTERN = RENDER_BY_TIME;
+        ledRenderPatterns[PATTERN]();
+        Serial.print("PATTERN = ");
+        Serial.println(PATTERN);
+      } else if(oneColor) {
+        Serial.println("RENDER_ONE_COLOR");
+        LedRenderPattern PATTERN = RENDER_ONE_COLOR;
+        ledRenderPatterns[PATTERN]();
+        Serial.print("PATTERN = ");
+        Serial.println(PATTERN);
+      } else {
+        Serial.println("Pattern = RENDER_RANDOMLY");
+        LedRenderPattern PATTERN = RENDER_RANDOMLY;
+        ledRenderPatterns[PATTERN]();
+        Serial.print("PATTERN = ");
+        Serial.println(PATTERN);
+      }
+  }
+ 
+  delay(2000);
+  trellis.pixels.show();
+}
+
+// Renders the colors by creation time.
+void renderByTime()
+{
+  //Serial.println("Rendering by time created: ");
+  // The colorString is already sorted when retrieved from server. We just render it.
+  renderColorString();
+}
+
+// Renders the colors ordered by color (Red -> Orange -> Blue -> Yellowgreen -> Green)
+void renderByColor()
+{
+  Serial.println("Rendering by color: ");
+  char x = '0';
+  int green = 0;
+  int yellowGreen = 0;
+  int blue = 0;
+  int orange = 0;
+  int red = 0;
+
+  // Count how often each color occurs in the colorString.
+  for (int i = 0; i < min(static_cast<int>(colorString.length()), static_cast<int>(NUM_LEDS)); i++) {
+    if (colorString[i] == '0') red +=1;
+    else if (colorString[i] == '1') orange +=1;
+    else if (colorString[i] == '2') blue +=1;
+    else if (colorString[i] == '3') yellowGreen +=1;
+    else if (colorString[i] == '4') green +=1;
+    else blue +=1;
+  }
+
+  //Check Values
+  Serial.print("red: ");
+  Serial.println(red);
+  
+  Serial.print("orange: ");
+  Serial.println(orange);
+
+  Serial.print("blue: ");
+  Serial.println(blue);
+
+  Serial.print("yellowGreen: ");
+  Serial.println(yellowGreen);
+
+  Serial.print("green: ");
+  Serial.println(green);
+  
+  
+  // Render each color
+  int ledIndex = 0;
+  ledIndex = renderColorRange(ledIndex, red, '0');
+  ledIndex = renderColorRange(ledIndex, orange, '1');
+  ledIndex = renderColorRange(ledIndex, blue, '2');
+  ledIndex = renderColorRange(ledIndex, yellowGreen, '3');
+  ledIndex = renderColorRange(ledIndex, green, '4');
+}
+
+// Renders the colors in random order.
+void renderRandomly()
+{
+  //Serial.println("Render randomly: ");
+  shuffleColorString();
+  renderColorString();
+}
+
+// Render the first color on all leds
+void renderOneColor() {
+  char c = '6';
+  if(colorString.length() > 0) c = colorString.charAt(0);
+    for (uint16_t i=0; i<trellis.pixels.numPixels(); i++) {
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (c == '0') trellis.pixels.setPixelColor(i,0xCC0000);//red
+      else if (c == '1') trellis.pixels.setPixelColor(i,0xB34700);//orange
+      else if (c == '2') trellis.pixels.setPixelColor(i,0x002DFF);//blue
+      else if (c == '3') trellis.pixels.setPixelColor(i,0xCCE100);//yellowGreen
+      else if (c == '4') trellis.pixels.setPixelColor(i,0x00CC00);//green
+      else trellis.pixels.setPixelColor(i,0x002DFF);//blue
+  }
+
+ }
+   
+}
+
+// Shuffles the order of the colors in the colorString.
+void shuffleColorString()
+{
+  int n = colorString.length();
+  if (n > 1)
+  {
+    int i;
+    for (i = 0; i < n - 1; i++)
+    {
+      int j = i + rand() / (RAND_MAX / (n - i) + 1);
+      char t = (colorString)[j];
+      (colorString)[j] = (colorString)[i];
+      (colorString)[i] = t;
+    }
+  }
+}
+
+// Writes a color block into the LED array.
+
+int renderColorRange(int index, int count, char x) {
+  for (int i = 0; i < count; i++, index++) {
+    if (x == '0') trellis.pixels.setPixelColor(i,0xCC0000);//red
+      else if (x == '1') trellis.pixels.setPixelColor(index,0xB34700);//orange
+      else if (x == '2') trellis.pixels.setPixelColor(index,0x002DFF);//blue
+      else if (x == '3') trellis.pixels.setPixelColor(index,0xCCE100);//yellowGreen
+      else if (x == '4') trellis.pixels.setPixelColor(index,0x00CC00);//green
+  }
+  return index;
+}
+
+// Writes the colors into the LED array.
+void renderColorString() {
+  for (int i = 0; i < min(static_cast<int>(colorString.length()), static_cast<int>(NUM_LEDS)); i++) {
+    char c = '6';
+    if(i < colorString.length()) c = colorString.charAt(i);
+    //Serial.println(c);
+    if (c == '0') trellis.pixels.setPixelColor(i,0xCC0000);
+      else if (c == '1') trellis.pixels.setPixelColor(i,0xB34700);
+      else if (c == '2') trellis.pixels.setPixelColor(i,0x002DFF);
+      else if (c == '3') trellis.pixels.setPixelColor(i,0xCCE100);
+      else if (c == '4') trellis.pixels.setPixelColor(i,0x00CC00);
+      else trellis.pixels.setPixelColor(i,0x002DFF);
+  }
+}
+
+
+//******************************** End LED rendering ********************************
---
+++ emoments_dotstar_crawler/emoments_dotstar_crawler.ino
@@ -0,0 +1,288 @@
+
+//Sketch by LizMyers@
+//Last updated: Aug 9, 2020
+
+//What it does: 
+// 1. Scrolls "Goooogle!". Colors match those in colorString.
+// 2. Single tap changes letters to single color
+// 3. Double tap restores to ordering color by time created
+//
+//    IDEA: Return tags: So we can show the related topic.
+//    Example: "#gtech" -> "Gooooooooogle!" 
+
+// Hardare:
+// a) Adafruit Huzzah Feather ESP32 (BT + WiFi)
+// b) Adafruit DotStar Feather Wing (12 x 6)
+// c) Adarfuit LIS3DH Accelerometer (accepts single/double tap)
+
+//for LIS3DH sensor
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+
+//for DotStar Matrix
+#include <Adafruit_GFX.h>
+#include <Adafruit_DotStarMatrix.h>
+#include <Adafruit_DotStar.h>
+#include <Fonts/TomThumb.h>
+
+//for BlueTooth
+#include <BLEDevice.h>
+#include <BLEServer.h>
+
+#if defined(ESP8266)
+  #define DATAPIN    13
+  #define CLOCKPIN   14
+#elif defined(__AVR_ATmega328P__)
+  #define DATAPIN    2
+  #define CLOCKPIN   4
+#elif defined(ARDUINO_NRF52832_FEATHER)
+  #define DATAPIN    7
+  #define CLOCKPIN   16
+#elif defined(TEENSYDUINO)
+  #define DATAPIN    9
+  #define CLOCKPIN   5
+#elif defined(ARDUINO_ARCH_WICED)
+  #define DATAPIN    PA4
+  #define CLOCKPIN   PB5
+#elif defined(ESP32)
+  #define DATAPIN    27
+  #define CLOCKPIN   13
+#else // // 32u4, M0, M4, nrf52840 and 328p
+  #define DATAPIN    11
+  #define CLOCKPIN   13
+#endif
+
+#define MAX_LEN 10
+#define SHIFTDELAY 120
+#define BRIGHTNESS 20
+
+// I2C Sensor
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+#define CLICKTHRESHHOLD 80
+
+Adafruit_DotStarMatrix matrix = Adafruit_DotStarMatrix(
+                                  12, 6, DATAPIN, CLOCKPIN,
+                                  DS_MATRIX_BOTTOM     + DS_MATRIX_LEFT +
+                                  DS_MATRIX_ROWS + DS_MATRIX_PROGRESSIVE,
+                                  DOTSTAR_BGR);
+
+const uint16_t primaryColors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255)
+};
+
+const uint16_t colorArray[] = {
+  matrix.Color(255, 0, 0),   // red
+  matrix.Color(200, 200, 0), //orange
+  matrix.Color(90, 200, 255), //blue
+  matrix.Color(180, 255, 0), //yellowGreen
+  matrix.Color(0, 255, 0),   //green
+  matrix.Color(180, 0, 255),  //purple, eMoments color
+};
+
+int yardstick = -70;//initializing wrap-around distance
+String colorString = "";
+String msgString = "eMoments!"; //initial msg shows we are up and running
+String msgStr1 = "";
+String msgStr2 = "";
+String msgStr3 = "";
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+bool bluetoothConnected = false;
+bool singleTap = false;// render one color
+//bool doubleTap = true; //default render by time
+
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    bluetoothConnected = true;
+    Serial.println("BT CONNECTED <<< :-) ");
+    Serial.print("Here is the colorString: ");
+    Serial.println(colorString);
+    delay(20);
+  };
+
+  void onDisconnect(BLEServer* pServer) {
+    bluetoothConnected = false;
+    colorString = "";
+    //msgString = "";
+    Serial.println("BT DISCONNECTED! <<< :-( ");
+  }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      String value = pCharacteristic->getValue().c_str();
+      Serial.println("Obtained colors from bluetooth:");
+      Serial.println(value);
+
+      Serial.print("ColorStringLength: ");
+      Serial.println(value.length());
+
+      colorString = value;
+
+      for(int i = 0; i < (colorString.length() - 5); i++) {
+        msgStr2.concat("o");
+      }
+      
+      msgString = ("G" + msgStr2 +"gle! ");
+      Serial.print("msgString: ");
+      Serial.println(msgString); 
+
+      Serial.print("msgStringLength: ");
+      Serial.println(msgString.length()); 
+
+      yardstick = ((msgString.length() + 60) * -1);
+      Serial.print("yardstick: ");
+      Serial.println(yardstick); 
+
+      Serial.print("singletap: ");
+      Serial.println(singleTap);
+    }
+};
+
+
+
+void setup() {
+  
+  //while (!Serial) delay(500); 
+ 
+  Serial.begin(115200);
+  Serial.println("\nDotstar Matrix Wing");
+
+   if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start");
+    while (1) yield();
+  }
+  Serial.println("LIS3DH found!");
+
+  lis.setRange(LIS3DH_RANGE_2_G);   // 2, 4, 8 or 16 G!
+  
+  Serial.print("Range = "); Serial.print(2 << lis.getRange());  
+  Serial.println("G");
+
+  // 0 = turn off click detection & interrupt
+  // 1 = single click only interrupt output
+  // 2 = double click only interrupt output, detect single click
+  // Adjust threshhold, higher numbers are less sensitive
+  lis.setClick(2, CLICKTHRESHHOLD);
+  delay(100);
+
+  matrix.begin();
+  matrix.setFont(&TomThumb);
+  matrix.setTextWrap(false);
+  matrix.setBrightness(BRIGHTNESS);
+
+  for (byte i = 0; i < 3; i++) {
+    matrix.fillScreen(primaryColors[i]);
+    matrix.show();
+    delay(500);
+  }
+  
+   // init bluetooth stuff
+  BLEDevice::init("Lizs DotStar");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic  *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pCharacteristic->setValue("DefaultValue");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Bluetooth setup done!");
+
+delay(500);
+
+}
+
+int x = matrix.width();
+int pass = 0;
+
+void loop() {
+
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 5);
+  msgStr2 = "";
+
+     for (int i = 0; i < MAX_LEN; i++) { //make sure the word finishes as --x has a limit
+        
+        char c = '6';
+
+        if(singleTap) { //singleTap = display single color or most recent
+          if(colorString.length() > 0) c = colorString.charAt(0);
+                  
+          //Serial.println(c);
+          
+          if (c == '0') matrix.setTextColor(colorArray[0]);      // red
+          else if (c == '1') matrix.setTextColor(colorArray[1]); // orange
+          else if (c == '2') matrix.setTextColor(colorArray[2]); // blue
+          else if (c == '3') matrix.setTextColor(colorArray[3]); // yellowGreen
+          else if (c == '4') matrix.setTextColor(colorArray[4]); // green
+          else matrix.setTextColor(colorArray[5]); //purple
+  
+          // write the letter
+          matrix.print(msgString[i]);
+        } else { 
+          
+          if(colorString.length() > 0) c = colorString.charAt(i);
+
+            singleTap = false;
+        
+          //Serial.println(c);
+        
+          if (c == '0') matrix.setTextColor(colorArray[0]);      // red
+          else if (c == '1') matrix.setTextColor(colorArray[1]); // orange
+          else if (c == '2') matrix.setTextColor(colorArray[2]); // blue
+          else if (c == '3') matrix.setTextColor(colorArray[3]); // yellowGreen
+          else if (c == '4') matrix.setTextColor(colorArray[4]); // green
+          else matrix.setTextColor(colorArray[5]); //purple
+  
+          // write the letter
+          matrix.print(msgString[i]);
+
+        } // end conditional
+     
+      }//end for loop
+
+  
+  if (--x < yardstick) {
+    x = matrix.width();
+  }
+
+  matrix.show();
+  delay(SHIFTDELAY);
+  
+  uint8_t click = lis.getClick();
+  if (click == 0) return;
+  if (! (click & 0x30)) return;
+  Serial.print("Click detected (0x"); Serial.print(click, HEX); Serial.print("): ");
+  
+  if (click && ("0x19" || "0x12" || "0x1A")) {
+    singleTap = true; 
+    return;
+  }
+
+  if (click && ("0x62" || "0x69")) {
+    singleTap = false;
+    return;
+  }
+ 
+  Serial.println();
+
+  delay(100);
+  return;
+  
+  
+} // MAIN LOOP
